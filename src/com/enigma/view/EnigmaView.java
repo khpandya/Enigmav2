2c3100e82a7b3be252d5f8f7bc2163a8972cc742
krishna <khpandya@mun.ca>
2022-04-04 11:27:19 -0230

    basic view

--- src/com/enigma/view/EnigmaView.java
+++ src/com/enigma/view/EnigmaView.java
@@ -1,17 +1,55 @@
 package com.enigma.view;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class EnigmaView extends JFrame {
     private JButton encrypt = new JButton("encrypt");
-    private JButton Decrypt = new JButton();
+    private JButton decrypt = new JButton("decrypt");
     private JTextArea inputText;
     private JTextArea outputText;
+    // TODO: can make slider adding and returning dynamic
     private JSlider slider1;
     private JSlider slider2;
     private JSlider slider3;
 
-    public EnigmaView() {
+    private void addElementsToMainPanel(JPanel mainPanel) {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800, 800);
+        mainPanel.add(encrypt);
+        mainPanel.add(decrypt);
+        mainPanel.add(inputText);
+        mainPanel.add(outputText);
+        mainPanel.add(slider1);
+        mainPanel.add(slider2);
+        mainPanel.add(slider3);
+    }
 
+    public EnigmaView() {
+        JPanel mainPanel = new JPanel();
+        addElementsToMainPanel(mainPanel);
+        this.add(mainPanel);
+    }
+
+    public String getInput() {
+        return inputText.getText();
+    }
+
+    public ArrayList<Integer> getRotorValues() {
+        ArrayList<Integer> values = new ArrayList<>();
+        values.add(slider1.getValue());
+        values.add(slider2.getValue());
+        values.add(slider3.getValue());
+        return values;
+    }
+
+    void addEncrpytListener(ActionListener listener) {
+        encrypt.addActionListener(listener);
+        decrypt.addActionListener(listener);
+    }
+
+    void displayError(String errorMessage) {
+        JOptionPane.showMessageDialog(this, errorMessage);
     }
 }
