74376d53edced7bc7beb673498c6061beabdfa49
krishna <khpandya@mun.ca>
2022-04-04 10:58:26 -0230

    deleted redundant files

--- src/com/enigma/view/EnigmaView.java
+++ src/com/enigma/view/EnigmaView.java
@@ -1,5 +1,17 @@
 package com.enigma.view;
 
-public class EnigmaView {
+import javax.swing.*;
 
+public class EnigmaView extends JFrame {
+    private JButton encrypt = new JButton("encrypt");
+    private JButton Decrypt = new JButton();
+    private JTextArea inputText;
+    private JTextArea outputText;
+    private JSlider slider1;
+    private JSlider slider2;
+    private JSlider slider3;
+
+    public EnigmaView() {
+
+    }
 }
